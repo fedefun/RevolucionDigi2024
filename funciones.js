497eaf005fedea2c4fd0350ed0441fcb6ef51734
fedefun <105380405+fedefun@users.noreply.github.com>
2024-05-18 21:58:12 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,21 @@
+// Primer ejercicio
+var nombre = "Franco";
+var apellido = "Lopez"
+
+
+
+console.log( nombre + " " + apellido );
+
+// Segundo ejercicio
+
+var a = 2;
+var b = 4;
+var suma;
+
+suma = a + b;
+
+console.log("La suma de los dos elementos es", suma);
+
+var c = suma + 10;
+
+console.log("La suma total junto con el ultimo elemento es ", c);
